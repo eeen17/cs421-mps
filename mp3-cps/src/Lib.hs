8d67eb0971c94d83d9c55c64c294ba940a11b52b
Ethan Chang <ethanytchang@gmail.com>
2026-03-15 22:55:23 -0700

    completed up to isSimple

--- mp3-cps/src/Lib.hs
+++ mp3-cps/src/Lib.hs
@@ -64,12 +64,28 @@ ctorShow (AppExp f e)     = "AppExp (" ++ ctorShow f ++ ") (" ++ ctorShow e ++ "
 --- ### `factk :: Integer -> (Integer -> t) -> t`
 
 factk :: Integer -> (Integer -> t) -> t
-factk = undefined
+-- factk = undefined
+factk = aux
+    where
+        aux 0 newk = newk 1  -- stop
+        aux x newk = aux (x-1) (\res -> newk (x * res))  -- res := result of aux(x-1). aux(x) = x * res
 
 --- ### `evenoddk :: [Integer] -> (Integer -> t) -> (Integer -> t) -> t`
 
+-- list and 2 continuations:
+-- cont 1: receives sum of all even elements of the list
+-- cont 2: receive sum of all odd elements of the list
+-- decides which cont to use based on last element's parity
 evenoddk :: [Integer] -> (Integer -> t) -> (Integer -> t) -> t
-evenoddk = undefined
+evenoddk = aux
+    where
+        aux [x] kEven kOdd
+            | even x = kEven x  -- if even, add last element to running even
+            | odd x = kOdd x -- if odd, add last element to running odd
+        aux (x:xs) kEven kOdd 
+            | even x = aux xs (\res -> kEven (res + x)) kOdd  -- if even, add x to the even sum result of aux(xs) and return (apply to previous running)
+            | odd x = aux xs kEven (\res -> kOdd (res + x)) -- vice versa
+
 
 --- Automated Translation
 --- ---------------------
@@ -79,8 +95,23 @@ gensym i = ("v" ++ show i, i + 1)
 
 --- ### Define `isSimple`
 
+
+-- IntExp Integer
+-- VarExp String
+-- LamExp String Exp
+-- IfExp Exp Exp Exp
+-- OpExp String Exp Exp
+-- AppExp Exp Exp
 isSimple :: Exp -> Bool
-isSimple = undefined
+isSimple exp =
+    case exp of
+        IntExp _ -> True
+        VarExp _ -> True
+        LamExp _ _ -> False -- ignore, just gonna return false for now
+        IfExp e1 e2 e3 -> isSimple e1 && isSimple e2 && isSimple e3
+        OpExp _ e1 e2 -> isSimple e1 && isSimple e2
+        AppExp _ _ -> False
+        
 
 --- ### Define `cpsExp` - Overview
 
